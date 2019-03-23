593879cae950dc7d5cf33108c1b48fa1e895d4a7
Caiosilva9 <caiosilva2207999@gmail.com>
2019-03-22 21:37:59 -0300

    ionic-introducao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, IonicPage } from 'ionic-angular';
+import { NavController, IonicPage, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +8,35 @@ import { NavController, IonicPage } from 'ionic-angular';
 @IonicPage()
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  nomes : any[] = [
+    {'nome' : 'Caio'},
+    {'nome' : 'Adryanno'},
+    {'nome' : 'Alessandra'},
+    {'nome' : 'Geycy'},
+    {'nome' : 'Regina'} 
+  ]
+
+  constructor(public navCtrl: NavController,
+    public alertCtrl: AlertController) {
 
   }
 
+  olaMundo(){
+    console.log('Olá Mundo Mobile!');
+    this.showAlert();
+  }
+
+  irParaTeste(){
+    this.navCtrl.push('TesteIonicPage')
+    // this.navCtrl.setRoot('TesteIonicPage') -  Sem seta para retorno
+  }
+
+  showAlert() {
+    const alert = this.alertCtrl.create({
+      title: 'Coé Rapaziada!',
+      subTitle: 'Vamos aprender Mobile?',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
