856a4ccd3e63ba99a4843f8e5231a4a082b865ad
Caiosilva8 <contacaiosilva@gmail.com>
2019-03-29 20:48:43 -0300

    Aula2, antes intervalo

---
+++ src/pages/form-contato/form-contato.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FormGroup, FormBuilder } from '@angular/forms';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-form-contato',
+  templateUrl: 'form-contato.html',
+})
+export class FormContatoPage {
+
+//1. declarar e importar
+  formGroup : FormGroup;
+
+
+  constructor(public navCtrl: NavController,
+     public navParams: NavParams,
+      public formBuilder : FormBuilder //2. Inserir o Form no construtor, importar
+    ) {
+
+  //3. Iniciar o FormBuilder com os campos do formulário do html  
+    this.formGroup = this.formBuilder.group({
+      nome: [''],
+      email: [''],
+      assunto: ['Suporte'],
+      mensagem: ['']
+    })
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad FormContatoPage');
+  }
+
+  enviarMensagem(){
+    console.log(this.formGroup.value  );
+  }
+
+}
